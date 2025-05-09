0b5610a8512843ec71b3d5c2b5734939dbe09ad6
Nikhil Singh <nikhil.s.singh19124@gmail.com>
2025-05-09 01:14:21 -0500

    changed directory

---
+++ Strings_practice.py/abc.py
@@ -0,0 +1,14 @@
+greeting = "Hello, World!"
+first_char = greeting[0]
+last_char = greeting[-1]
+print(f"First character: {first_char}")
+print(f"Last character: {last_char}")
+slice = greeting[1:5]
+print(f"Slice: {slice}")
+length = len(greeting)
+print(f"Length: {length}")
+full = "Hello" + " " + "World!"
+print(f"Full: {full}")
+laugh = "Ha" * 3
+print(f"Laugh: {laugh}")
+print("Hello" in greeting)
