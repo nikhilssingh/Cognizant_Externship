abaa0c2430e2865ad8d535d6bf0c6d6c1d883a78
nikhilssingh <nikhil.s.singh19124@gmail.com>
2025-05-13 14:08:17 -0500

    Added assingment 5 not 4(correction)

--- Assignment5/Task4.py
+++ Assignment5/Task4.py
@@ -2,10 +2,10 @@ n = int(input("Enter a number for which factorial should be calculated: "))
 
 def factorial(n):
     if n==0 or n==1:
+    # base case
         return 1
     else:
         return n*factorial(n-1)
 
 print(factorial(n))
         
-    
