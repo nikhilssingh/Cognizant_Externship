213f61a7368c2e3849cc56be473c8aa1793a0cd1
nikhilssingh <nikhil.s.singh19124@gmail.com>
2025-05-13 16:24:38 -0500

    Added negative number checking in project 5

--- Project5/RecursionFunctionality.py
+++ Project5/RecursionFunctionality.py
@@ -6,7 +6,8 @@ def factorial(n):
         return 1
     else:
         return n*factorial(n-1)
-
+    # using recursion to calculate factorial of a number
+    
 def fibonacci(n):
     if n==0:
         return 0
@@ -14,7 +15,7 @@ def fibonacci(n):
         return 1
     else:
         return fibonacci(n-1) + fibonacci(n-2)
-    
+    # using recursion to calculate fibonacci number of a posiiton 
 def fractal_pattern(length):
     if length<10:
         return
@@ -25,35 +26,45 @@ def fractal_pattern(length):
     fractal_pattern(length-15)
     turtle.left(30)
     turtle.backward(length)
-
+    # using recursion to draw a fractal pattern of a tree using turtle library
 while True:
     print("Menu of Recursive Functions:\n")
     print("1. Calculate the factorial of a number")
     print("2. Find the nth Fibonacci number")
     print("3. Draw a recursive fractal pattern")
     print("4. Exit")
-    
+    # Ensures the menu is returned after every iteration
     option = int(input("Choose an option by entering the option number(1-4): "))
 
     if option==1:
         n = int(input("Enter a number to find its factorial: "))
-        print(f"The factorial of {n} is {factorial(n)}.\n")
+        if n<0:
+            print("Enter a positive number only.\n")
+        else:
+            print(f"The factorial of {n} is {factorial(n)}.\n")
     elif option==2:
         n = int(input("Enter the position of the Fibonacci number: "))
-        print(f"The {n}th Fibonacci number is {fibonacci(n)}.\n")
+        if n<0:
+            print("Enter a positive number only.\n")
+        else:
+            print(f"The {n}th Fibonacci number is {fibonacci(n)}.\n")
     elif option==3:
         length = int(input("Enter the length of the fractal pattern tree: "))
-        turtle.speed("fastest")
-        turtle.left(90)
-        turtle.penup()
-        turtle.goto(0,-200)
-        turtle.pendown()
-        turtle.color("blue")
-        fractal_pattern(length)
-        turtle.done()
+        if n<0:
+            print("Enter a positive number only.\n")
+        else:
+            turtle.speed("fastest")
+            turtle.left(90)
+            turtle.penup()
+            turtle.goto(0,-200)
+            turtle.pendown()
+            turtle.color("blue")
+            fractal_pattern(length)
+            turtle.done()
     elif option==4:
         print("Exiting Program.")
         break
+        # exits program after option 4 is selected
     else:
         print("Invalid option. Please choose (1-4).\n")
     
