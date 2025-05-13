cebce9dc618f2424824ee2a49339921f1fb4b4be
nikhilssingh <nikhil.s.singh19124@gmail.com>
2025-05-13 14:39:40 -0500

    updated assingment 5

--- Assignment5/Task4.py
+++ Assignment5/Task4.py
@@ -1,11 +1,21 @@
-n = int(input("Enter a number for which factorial should be calculated: "))
+num1 = int(input("Enter a number for which factorial should be calculated: "))
+num2 = int(input("Enter a number for which fibonacci should be calculated: "))
 
-def factorial(n):
-    if n==0 or n==1:
+def factorial(num1):
+    if num1==0 or num1==1:
     # base case for 0 and 1
         return 1
     else:
-        return n*factorial(n-1)
+        return num1*factorial(num1-1)
 
-print(factorial(n))
+print(f"The factorial of {num1} is {factorial(num1)}.")
         
+def fibonacci(num2):
+    if num2==0:
+        return 0
+    elif num2==1:
+        return 1
+    else:
+        return fibonacci(num2-1) + fibonacci(num2-2)
+    
+print(f"The fibonacci of {num2} is {fibonacci(num2)}.")
